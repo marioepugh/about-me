fa75a536d3d61feb2afcf46d0573945ee8abbc36
marioepugh <marioepugh@gmail.com>
2017-07-11 17:54:20 -0700

    I finished question number 1

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 'use strict';
 
-alert('Hey is this thing on?');
+alert('Lets play a yes or no game based on my about me page');
+var age = prompt('Am I older than 45 yrs old?');
+
+if(age.toLowerCase() == 'yes') {
+  console.log('Correct! I am 46 yrs old.');
+} else {
+  console.log('wrong');
+}
