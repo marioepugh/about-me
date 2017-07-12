b8d47972a36b536f2e98ed381f40d0f8c7ba0de8
marioepugh <marioepugh@gmail.com>
2017-07-11 18:24:06 -0700

    Finished all five questions

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ alert('Lets play a yes or no game based on my about me page');
 var age = prompt('Am I older than 45 yrs old?');
 
 if(age.toLowerCase() == 'yes') {
-  console.log('Correct! I am 46 yrs old.');
+  alert('Correct! I am 46 yrs old.');
 } else {
-  console.log('wrong');
+  alert('wrong');
+}
+var sun = prompt('Did I spent 89 days underwater on a submarine?');
+
+if(sun.toLowerCase() == 'yes') {
+  alert('Correct!');
+} else {
+  alert('wrong');
+}
+
+var treehouse = prompt('Am I getting married at Tree House Point');
+
+if(treehouse.toLowerCase() == 'yes') {
+  alert('Correct!');
+} else {
+  alert('wrong');
+}
+var jeep = prompt('Do I have a Jeep Wrangler?');
+
+if(jeep.toLowerCase() == 'yes') {
+  alert('Correct!');
+} else {
+  alert('wrong');
+}
+
+var amsterdam = prompt('Do I love to travel?');
+
+if(amsterdam.toLowerCase() == 'yes') {
+  alert('Correct!');
+} else {
+  alert('wrong');
 }
